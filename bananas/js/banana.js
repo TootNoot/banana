61eb6d072bf95ecd23f6e3b028a6e8f3a4de8592
Kevin <54513686+TootNoot@users.noreply.github.com>
2020-05-08 22:06:09 -0400

    Add files via upload

---
+++ bananas/js/banana.js
@@ -0,0 +1,12 @@
+function function1(){
+    document.getElementById("mainImage").src = "media/banana/1.png";
+}
+function function2(){
+    document.getElementById("mainImage").src = "media/banana/2.png";
+}
+function function3(){
+    document.getElementById("mainImage").src = "media/banana/3.png";
+}
+function function4(){
+    document.getElementById("mainImage").src = "media/banana/4.png";
+}
