7824e83996d879718220eb922215c0fc65a8927f
Kevin <54513686+TootNoot@users.noreply.github.com>
2020-08-14 20:37:24 -0400

    Add files via upload

---
+++ bananas/js/plastic.js
@@ -0,0 +1,9 @@
+function function1(){
+    document.getElementById("mainImage").src = "media/plastic/1.png";
+}
+function function2(){
+    document.getElementById("mainImage").src = "media/plastic/2.png";
+}
+function function3(){
+    document.getElementById("mainImage").src = "media/plastic/3.png";
+}
